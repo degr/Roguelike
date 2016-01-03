7eba8ff5b85f2bb9cb4bdedf5eef3f133ff0f8a4
degr <degradator@bk.ru>
2016-01-03 15:41:19 +0300

    Initial commit

---
+++ src/org/forweb/roguelike/dao/AbstractDao.java
@@ -0,0 +1,7 @@
+package org.forweb.roguelike.dao;
+
+/**
+ * Created by Ror on 03.01.2016.
+ */
+public class AbstractDao {
+}
---
+++ src/org/forweb/roguelike/entity/AbstractEntity.java
@@ -0,0 +1,7 @@
+package org.forweb.roguelike.entity;
+
+/**
+ * Created by Ror on 03.01.2016.
+ */
+public interface AbstractEntity {
+}
