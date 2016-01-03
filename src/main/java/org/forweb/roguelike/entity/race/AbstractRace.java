e8e370cc2d918a034b72aa325a35409e4391618b
degr <degradator@bk.ru>
2016-01-03 19:56:12 +0300

    f

--- src/main/java/org/forweb/roguelike/entity/race/AbstractRace.java
+++ src/main/java/org/forweb/roguelike/entity/race/AbstractRace.java
@@ -1,10 +1,30 @@
 package org.forweb.roguelike.entity.race;
 
 import org.forweb.roguelike.entity.AbstractEntity;
+import org.forweb.roguelike.entity.utils.Statistics;
 
 /**
  * Created by Ror on 03.01.2016.
  */
-public class AbstractRace extends AbstractEntity {
+public abstract class AbstractRace extends AbstractEntity {
     
+    public AbstractRace(Statistics min, Statistics limitations, Statistics limit) {
+        this.min = min;
+        this.limitations = limitations;
+        this.limit = limit;
+    }
+    
+    protected Statistics min;
+    protected Statistics limitations;
+    protected Statistics limit;
+    
+    public Statistics getMin(){
+        return min;
+    }
+    public Statistics getLimitations(){
+        return limitations;
+    }
+    public Statistics getLimit(){
+        return limit;
+    }
 }
