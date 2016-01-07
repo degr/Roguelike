f15d4e74521e11c70fd03bcfe4caa681ccdcdf0c
degr <degradator@bk.ru>
2016-01-07 14:55:21 +0300

    k

--- src/main/java/org/forweb/roguelike/entity/size/category/small.java
+++
@@ -1,7 +0,0 @@
-package org.forweb.roguelike.entity.size.category;
-
-/**
- * Created by Ror on 07.01.2016.
- */
-public class small {
-}
--- src/main/java/org/forweb/roguelike/map/map/object/tree.java
+++
@@ -1,7 +0,0 @@
-package org.forweb.roguelike.map.map.object;
-
-/**
- * Created by Ror on 07.01.2016.
- */
-public class tree {
-}
